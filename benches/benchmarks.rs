2b26933e51063c1b5c06758b6b38dfd0d8769b91
J/A <archer884@gmail.com>
2018-05-14 17:06:04 -0500

    Test performance with alternate hashers

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,5 +1,7 @@
 #![feature(test)]
 
+extern crate fnv;
+extern crate fxhash;
 extern crate groupby;
 extern crate test;
 
@@ -7,9 +9,31 @@ use groupby::GroupBy;
 use test::Bencher;
 
 #[bench]
-fn measure_1_10_1000(b: &mut Bencher) {
+fn default(b: &mut Bencher) {
+    let seq: Vec<i32> = (1..=10).cycle().take(1000).collect();
+    b.iter(|| test::black_box(seq.iter().cloned().group_by(|&x| x)));
+}
+
+#[bench]
+fn fnv_hash(b: &mut Bencher) {
     let seq: Vec<i32> = (1..=10).cycle().take(1000).collect();
     b.iter(|| {
-        test::black_box(seq.iter().cloned().group_by(|&x| x))
+        test::black_box(
+            seq.iter()
+                .cloned()
+                .group_by_with_hasher(|&x| x, fnv::FnvBuildHasher::default()),
+        )
+    });
+}
+
+#[bench]
+fn fx_hash(b: &mut Bencher) {
+    let seq: Vec<i32> = (1..=10).cycle().take(1000).collect();
+    b.iter(|| {
+        test::black_box(
+            seq.iter()
+                .cloned()
+                .group_by_with_hasher(|&x| x, fxhash::FxBuildHasher::default()),
+        )
     });
 }
